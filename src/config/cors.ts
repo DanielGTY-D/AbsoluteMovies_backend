936a90ea42c054d08a1b7a3170bae904465171e6
yael torres <yaelgtorres25@gmail.com>
2025-08-24 19:33:43 -0600

    Ya se puede iniciar sesion

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -6,6 +6,7 @@ dotenv.config();
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [];
+
     whiteList.push(process.env.FRONTEND_URL);
     if (process.argv[2] === "--api") {
       whiteList.push(undefined);
